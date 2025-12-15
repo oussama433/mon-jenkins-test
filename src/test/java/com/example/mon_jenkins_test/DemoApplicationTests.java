8f872015ab36586bfcd13c9a2757b71348b7c558
oussama433 <oussama.mrayah@esprit.tn>
2025-12-15 21:48:39 +0000

    Initial commit: Spring Boot app with Jenkins pipeline

---
+++ src/test/java/com/example/mon_jenkins_test/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mon_jenkins_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
